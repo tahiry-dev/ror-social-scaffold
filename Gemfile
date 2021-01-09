64a7dee367cbb4a89c1e77e59870ba3fc931dadd
tahiry-dev <tahirycontact@gmail.com>
2021-01-09 11:10:37 +0300

    fix rubocop errors

--- Gemfile
+++ Gemfile
@@ -67,6 +67,5 @@ end
 
 gem 'omniauth-facebook'
 
-
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 # gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -51,9 +50,10 @@ class User < ApplicationRecord
   # rubocop:enable Lint/Void
 
   def self.from_omniauth(auth)
-  name_split = auth.info.name.split(" ")
-  user = User.where(email: auth.info.email).first
-  user ||= User.create!(provider: auth.provider, uid: auth.uid, name: name_split[1], email: auth.info.email, password: Devise.friendly_token[0, 20])
+    name_split = auth.info.name.split(' ')
+    user = User.where(email: auth.info.email).first
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, name: name_split[1], email: auth.info.email,
+                          password: Devise.friendly_token[0, 20])
     user
   end
 end
